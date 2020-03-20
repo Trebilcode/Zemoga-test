190b94917b1584f32a276d78204ffdec58a954c0
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2020-03-20 16:47:56 -0500

    Set app to push to heroku, Procfile added to root

---
+++ build/precache-manifest.e8d3fdc9301b8df10e114ce13ce96f3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c3c9148861da97b845d10776a46010",
+    "url": "/index.html"
+  },
+  {
+    "revision": "198e56750cbb334d9561",
+    "url": "/static/css/main.5d03c2b2.chunk.css"
+  },
+  {
+    "revision": "1212aac839142850facd",
+    "url": "/static/js/2.9ac01892.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.9ac01892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198e56750cbb334d9561",
+    "url": "/static/js/main.b14ad65c.chunk.js"
+  },
+  {
+    "revision": "1be85c54fc0314d93946",
+    "url": "/static/js/runtime-main.7701514d.js"
+  },
+  {
+    "revision": "9dfd23d3ce92caac82fbed92a62ba5dc",
+    "url": "/static/media/pope.9dfd23d3.png"
+  }
+]);
